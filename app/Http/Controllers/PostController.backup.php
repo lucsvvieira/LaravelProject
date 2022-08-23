ee8b49cc7c2e56d797b6cd73bc2c7b9e7caf7db7
Lucas Vieira <vieira07lucas@gmail.com>
2022-08-23 19:19:22 -0300

    Implementando controller do tipo Resource

---
+++ app/Http/Controllers/PostController.backup.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function create(Request $request) {
+        $new_post = [
+            'title' => 'Meu primeiro post',
+            'content' => 'Conteúdo Qualquer',
+            'author' => 'Lucas'
+        ];
+
+        // Forma mais convencional de criar um registro no banco.
+        // $post = new Post($new_post);
+        // $post->save();
+
+        $post = new Post();
+
+        $post->title = 'Meu segundo post';
+        $post->content = 'Conteúdo Qualquer';
+        $post->author = 'Lucas Vieira';
+        $post->save();
+
+        dd($post);
+    }
+
+    public function read(Request $r) {
+        
+        $post = new Post();
+
+        $post = $post->find(2);
+
+        return $post;
+    }
+
+    public function all(Request $r) {
+        $posts = Post::all();
+
+        return $posts;
+    }
+
+    public function update(Request $request) {
+
+        $posts = Post::where('id', '>', 1)->update([
+            'author' => 'Lucas Vieira',
+            'title' => 'Alterado'
+        ]);
+        
+        return $posts;
+
+    }
+
+    public function delete(Request $request) {
+
+        $post = Post::where('id', '>', 0)->delete();
+        return $post;
+
+    }
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,26 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function create(Request $request) {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // No futuro, vai só exibir um formulário.
+        
         $new_post = [
             'title' => 'Meu primeiro post',
             'content' => 'Conteúdo Qualquer',
@@ -25,39 +44,72 @@ class PostController extends Controller
         $post->author = 'Lucas Vieira';
         $post->save();
 
-        dd($post);
+        return $post;
     }
 
-    public function read(Request $r) {
-        
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // No futuro, receberá um post com um novo recurso.
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
         $post = new Post();
 
-        $post = $post->find(2);
+        $post = $post->find($id);
 
         return $post;
     }
 
-    public function all(Request $r) {
-        $posts = Post::all();
-
-        return $posts;
-    }
-
-    public function update(Request $request) {
-
-        $posts = Post::where('id', '>', 1)->update([
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $posts = Post::find($id)->update([
             'author' => 'Lucas Vieira',
             'title' => 'Alterado'
         ]);
         
         return $posts;
-
     }
 
-    public function delete(Request $request) {
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 
-        $post = Post::where('id', '>', 0)->delete();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id)->delete();
         return $post;
-
     }
 }
